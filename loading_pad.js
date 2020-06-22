cf307ced69b279bb6acc38e8325caad32f49389f
Samir Sayyad <samir.saiad@gmail.com>
2020-06-22 13:08:00 +0430

    loading pad init

---
+++ loading_pad.js
@@ -0,0 +1,17 @@
+var eejs = require('ep_etherpad-lite/node/eejs/');
+
+
+var before = "<div class='loading_message'>";
+var during = "I am plugin"
+var after = "</div>";
+var html = before + during + after;
+
+
+exports.eejsBlock_loading = function (hook_name, args, cb) {
+  args.content = args.content + html;
+}
+
+exports.eejsBlock_styles = function (hook_name, args, cb) {
+  args.content = args.content + "<link href='../static/plugins/ep_loading_pad/static/css/loading_pad.css' rel='stylesheet'>";
+  return cb();
+}
