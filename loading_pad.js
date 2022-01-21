8077ca5ed8e26d1456c7b669894c8a45a94ac982
Hossein <marzban98@gmail.com>
2022-01-21 15:23:39 +0330

    feat: loading for timeslider

--- loading_pad.js
+++ loading_pad.js
@@ -1,18 +1,20 @@
-var eejs = require('ep_etherpad-lite/node/eejs/');
-
-
-var before = "<div class='loading_message'>";
-var during = "I am plugin"
-var after = "</div>";
-var html = before + during + after;
-
+const eejs = require('ep_etherpad-lite/node/eejs/');
 
 exports.eejsBlock_loading = (hook_name, args, cb) =>{
   args.content = eejs.require("ep_loading_pad/templates/loading.html") ;
   return {};
 }
 
+exports.eejsBlock_timesliderLoading = (hook_name, args, cb) =>{
+  args.content = eejs.require("ep_loading_pad/templates/loading.html") ;
+  return {};
+}
+
 exports.eejsBlock_styles = (hook_name, args, cb) =>{
+  args.content = args.content + "<link href='../static/plugins/ep_loading_pad/static/css/loading_pad.css' rel='stylesheet'>";
+  return {};
+}
+exports.eejsBlock_timesliderStyles = (hook_name, args, cb) =>{
   args.content = args.content + "<link href='../static/plugins/ep_loading_pad/static/css/loading_pad.css' rel='stylesheet'>";
   return {};
 }
